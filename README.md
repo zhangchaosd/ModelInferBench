8962eec357085e8e85714bb725bb51805cafdd95
zhangchaosd <zhangchaogoo@gmail.com>
2023-08-26 00:45:15 +0800

    Update Readme and mps

--- README.md
+++ README.md
@@ -5,20 +5,32 @@ python test_python.py
 
 ## Windows
 
+My PC:
 13900
 32G DDR4 3000
-1070ti
+1070ti + A770 16G
+
+My Mac:
+MacBook Pro 16-inch, 2021
+Apple M1 Pro
+A2485
+13.5 (22G74)
+
 
 efficientnet_b4  1, 3, 224, 224
 
-batch_size 1
-cpu 171.536 ms
-gpu 10.715 ms
-onnx cpu 39.876 ms
-onnx gpu 23.337 ms
+Infer 20 times, and take the average of the last 10 times.
+(ms)
 
-batch_size 4
-cpu 513.850 ms
-gpu 25.534 ms
-onnx cpu 46.736 ms
-onnx gpu 18.768 ms
+| PC | batch_size 1 | batch_size 4 |
+|:------:|:----:|:------:|
+| PyTorch cpu | 172 ms | 514 ms |
+| PyTorch cuda | 11 ms | 26 ms |
+| Python onnxruntime cpu | 40 ms | 47 ms |
+| Python onnxruntime cuda | 23 ms | 19 ms |
+
+| MacBook | batch_size 1 | batch_size 4 |
+|:------:|:----:|:------:|
+| PyTorch cpu | 887 ms | 1207 ms |
+| PyTorch mps | 37 ms | 39 ms |
+| Python onnxruntime cpu | 59 ms | 208 ms |
--- test_python.py
+++ test_python.py
@@ -1,71 +1,91 @@
-import torch
-import onnx
-import torchvision
 import time
-import onnxruntime as ort
+
 import numpy as np
+import onnx
+import onnxruntime as ort
+import torch
+import torchvision
 
 onnx_path = "model.onnx"
 batch_size = 4
+input_size = (batch_size, 3, 224, 224)
+# model = torchvision.models.efficientnet_v2_l()
+model = torchvision.models.efficientnet_b4()
+
 
 def export_onnx(model):
     model.eval().to("cpu")
-    input = torch.randn(batch_size,3,224,224).to("cpu")
-    torch.onnx.export(model,
+    input = torch.randn(*input_size).to("cpu")
+    torch.onnx.export(
+        model,
         input,
         onnx_path,
         input_names=["input"],
         output_names=["output"],
-        dynamic_axes={
-            "input": {0: "batch_size"},
-            "output": {0: "batch_size"}
-            }
+        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
     )
     model = onnx.load(onnx_path)
     onnx.checker.check_model(model)
 
+
+results = []
+
+
 def test(model, device):
     model.eval().to(device)
-    input = torch.randn(batch_size,3,224,224).to(device)
+    input = torch.randn(*input_size).to(device)
 
-    total_time = 0.
+    total_time = 0.0
     epoch = 20
     for i in range(epoch):
         start_time = time.time()
         output = model(input)
         end_time = time.time()
         elapsed_time = (end_time - start_time) * 1000
-        print(f"Running time: {elapsed_time:.3f} ms")
+        print(f"Running time: {elapsed_time:.0f} ms")
         if i >= epoch - 10:
             total_time += elapsed_time
     total_time /= 10
-    print(f"Device: {device}. Average running time in last 10 epochs: {total_time:.3f} ms")
+    result = (
+        f"Device: {device}. Average running time in last 10 epochs: {total_time:.0f} ms"
+    )
+    print(result)
+    results.append(result)
+
 
 def test_onnxruntime(provider):
     sess = ort.InferenceSession(onnx_path, providers=[provider])
-    input = np.random.rand(batch_size, 3, 224, 224).astype(np.float32)
-    total_time = 0.
+    input = np.random.rand(*input_size).astype(np.float32)
+    total_time = 0.0
     epoch = 20
     for i in range(epoch):
         start_time = time.time()
-        output = sess.run(["output"], {"input":input})[0]
+        output = sess.run(["output"], {"input": input})[0]
         end_time = time.time()
         elapsed_time = (end_time - start_time) * 1000
-        print(f"Running time: {elapsed_time:.3f} ms")
+        print(f"Running time: {elapsed_time:.0f} ms")
         if i >= epoch - 10:
             total_time += elapsed_time
     total_time /= 10
-    print(f"Onnxruntime: {provider}. Average running time in last 10 epochs: {total_time:.3f} ms")
+    result = f"Onnxruntime: {provider}. Average running time in last 10 epochs: {total_time:.0f} ms"
+    print(result)
+    results.append(result)
 
 
-# model = torchvision.models.efficientnet_v2_l()
-model = torchvision.models.efficientnet_b4()
 export_onnx(model)
 
-devices = ["cpu", "cuda"]
+devices = ["cpu"]
+if torch.cuda.is_available():
+    devices.append("cuda")
+if torch.backends.mps.is_available():
+    devices.append("mps")
 for device in devices:
     test(model, device)
 
-providers = ["CPUExecutionProvider", "CUDAExecutionProvider"]
-for provider in providers:
+available_providers = ort.get_available_providers()
+for provider in available_providers:
     test_onnxruntime(provider)
+
+print("Results:")
+for result in results:
+    print(result)
