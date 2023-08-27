b3f7728cf17879b42ebbe69351aaf235ede148a8
Chao Zhang <zhangchaogoo@gmail.com>
2023-08-27 19:13:18 +0800

    Update python

--- test_python.py
+++ test_python.py
@@ -5,6 +5,7 @@ import onnx
 import onnxruntime as ort
 import torch
 import torchvision
+from openvino.runtime import Core
 
 onnx_path = "model.onnx"
 batch_size = 1
@@ -30,142 +31,104 @@ def export_onnx(model):
     model = onnx.load(onnx_path)
     onnx.checker.check_model(model)
 
-
 results = []
 
+def test_Pytorch(model):
+    devices = ["cpu"]
+    if torch.cuda.is_available():
+        devices.append("cuda")
+    if torch.backends.mps.is_available():
+        devices.append("mps")
+    for device in devices:
+        model.eval().to(device)
+        input = torch.randn(*input_size).to(device).to(torch_type)
 
-def test(model, device):
-    model.eval().to(device)
-    input = torch.randn(*input_size).to(device).to(torch_type)
-
-    total_time = 0.0
-    epoch = 20
-    for i in range(epoch):
-        start_time = time.time()
-        output = model(input)
-        end_time = time.time()
-        elapsed_time = (end_time - start_time) * 1000
-        print(f"Running time: {elapsed_time:.0f} ms")
-        if i >= epoch - 10:
-            total_time += elapsed_time
-    total_time /= 10
-    result = (
-        f"Device: {device}. Average running time in last 10 epochs: {total_time:.0f} ms"
-    )
-    print(result)
-    results.append(result)
+        total_time = 0.0
+        epoch = 20
+        for i in range(epoch):
+            start_time = time.time()
+            output = model(input)
+            end_time = time.time()
+            elapsed_time = (end_time - start_time) * 1000
+            print(f"Running time: {elapsed_time:.0f} ms")
+            if i >= epoch - 10:
+                total_time += elapsed_time
+        total_time /= 10
+        result = (
+            f"Device: {device}. Average running time in last 10 epochs: {total_time:.0f} ms"
+        )
+        print(result)
+        results.append(result)
 
 
-def test_onnxruntime(provider):
-    sess = ort.InferenceSession(onnx_path, providers=[provider])
-    input = np.random.rand(*input_size).astype(np_type)
-    total_time = 0.0
-    epoch = 20
-    for i in range(epoch):
-        start_time = time.time()
-        output = sess.run(["output"], {"input": input})[0]
-        end_time = time.time()
-        elapsed_time = (end_time - start_time) * 1000
-        print(f"Running time: {elapsed_time:.0f} ms")
-        if i >= epoch - 10:
-            total_time += elapsed_time
-    total_time /= 10
-    result = f"Onnxruntime: {provider}. Average running time in last 10 epochs: {total_time:.0f} ms"
-    print(result)
-    results.append(result)
+def test_ONNXRuntime():
+    available_providers = ort.get_available_providers()
+    print(f"ONNX Runtime available_providers: {available_providers}")
+    for provider in available_providers:
+        sess = ort.InferenceSession(onnx_path, providers=[provider])
+        input = np.random.rand(*input_size).astype(np_type)
+        total_time = 0.0
+        epoch = 20
+        for i in range(epoch):
+            start_time = time.time()
+            output = sess.run(["output"], {"input": input})[0]
+            end_time = time.time()
+            elapsed_time = (end_time - start_time) * 1000
+            print(f"Running time: {elapsed_time:.0f} ms")
+            if i >= epoch - 10:
+                total_time += elapsed_time
+        total_time /= 10
+        result = f"Onnxruntime: {provider}. Average running time in last 10 epochs: {total_time:.0f} ms"
+        print(result)
+        results.append(result)
 
 def test_onnxruntime_directml():
     # pip install onnxruntime-directml
     pass
 
 def test_OpenVINO():
-    # python -m pip install openvino
-    from openvino.preprocess import PrePostProcessor, ResizeAlgorithm
-    from openvino.runtime import Core, Layout, Type
-    print(Core().available_devices)
-    device_name = "CPU"  # change here
-    # device_name = "GPU"  # change here
-    # device_name = "GPU.0"  # A770
-    # device_name = "GPU.1"  # 1070Ti
+    devices = Core().available_devices
+    print(f"OpenVINO available devices: {devices}")
+    for device_name in devices:
+        core = Core()
+        # Read a model
+        # (.xml and .bin files) or (.onnx file)
+        model = core.read_model(onnx_path)
 
-    # Step 1. Initialize OpenVINO Runtime Core
-    core = Core()
-    # Step 2. Read a model
-    # (.xml and .bin files) or (.onnx file)
-    model = core.read_model(onnx_path)
+        if len(model.inputs) != 1:
+            print('Sample supports only single input topologies')
+            return
 
-    if len(model.inputs) != 1:
-        print('Sample supports only single input topologies')
-        return -1
+        if len(model.outputs) != 1:
+            print('Sample supports only single output topologies')
+            return
 
-    if len(model.outputs) != 1:
-        print('Sample supports only single output topologies')
-        return -1
+        input_tensor = np.random.rand(*input_size).astype(np_type)
+        compiled_model = core.compile_model(model, device_name)
 
-    # Step 4. Apply preprocessing
-    # ppp = PrePostProcessor(model)
+        total_time = 0.0
+        epoch = 20
+        for i in range(epoch):
+            start_time = time.time()
+            _ = compiled_model.infer_new_request({0: input_tensor})
+            end_time = time.time()
+            elapsed_time = (end_time - start_time) * 1000
+            print(f"OpenVINI {device_name} {i} Running time: {elapsed_time:.0f} ms")
+            if i >= epoch - 10:
+                total_time += elapsed_time
+        total_time /= 10
+        result = f"OpenVINI: {device_name}. Average running time in last 10 epochs: {total_time:.0f} ms"
+        print(result)
+        results.append(result)
+        # This sample is an API example, for any performance measurements please use the dedicated benchmark_app tool
+        #https://docs.openvino.ai/2023.0/openvino_inference_engine_tools_benchmark_tool_README.html
 
-    input_tensor = np.random.rand(*input_size).astype(np_type)
-
-    # 1) Set input tensor information:
-    # - input() provides information about a single model input
-    # - reuse precision and shape from already available `input_tensor`
-    # - layout of data is 'NHWC'
-    # ppp.input().tensor() \
-    #     .set_shape(input_size) \
-    #     .set_element_type(Type.f32) \
-    #     .set_layout(Layout('NCHW'))  # noqa: ECE001, N400
-
-    # 2) Adding explicit preprocessing steps:
-    # - apply linear resize from tensor spatial dims to model spatial dims
-    # ppp.input().preprocess().resize(ResizeAlgorithm.RESIZE_LINEAR)
-
-    # 3) Here we suppose model has 'NCHW' layout for input
-    # ppp.input().model().set_layout(Layout('NCHW'))
-
-    # 4) Set output tensor information:
-    # - precision of tensor is supposed to be 'f32'
-    # ppp.output().tensor().set_element_type(Type.f32)
-
-    # 5) Apply preprocessing modifying the original 'model'
-    # model = ppp.build()
-    compiled_model = core.compile_model(model, device_name)
-
-    total_time = 0.0
-    epoch = 20
-    for i in range(epoch):
-        start_time = time.time()
-        _ = compiled_model.infer_new_request({0: input_tensor})
-        end_time = time.time()
-        elapsed_time = (end_time - start_time) * 1000
-        print(f"OpenVINI {i} Running time: {elapsed_time:.0f} ms")
-        if i >= epoch - 10:
-            total_time += elapsed_time
-    total_time /= 10
-    result = f"OpenVINI: {device_name}. Average running time in last 10 epochs: {total_time:.0f} ms"
-    print(result)
-    results.append(result)
-
-    # print('This sample is an API example, for any performance measurements please use the dedicated benchmark_app tool\n')
-    #https://docs.openvino.ai/2023.0/openvino_inference_engine_tools_benchmark_tool_README.html
-    return 0
 
 export_onnx(model)
 
-devices = ["cpu"]
-if torch.cuda.is_available():
-    devices.append("cuda")
-if torch.backends.mps.is_available():
-    devices.append("mps")
-for device in devices:
-    test(model, device)
-
-available_providers = ort.get_available_providers()
-for provider in available_providers:
-    test_onnxruntime(provider)
-
+test_Pytorch(model)
+test_ONNXRuntime()
 test_OpenVINO()
 
 print("Results:")
-for result in results:
-    print(result)
+list(map(print, results))
